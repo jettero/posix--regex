e02ec6561b7745c001cab488ca374e28493ad452
Paul Miller <jettero@voltar.org>
2008-05-18 09:18:54 -0400

    yet another patch just for bsd

--- Regex.pm
+++ Regex.pm
@@ -17,7 +17,7 @@ our %EXPORT_TAGS = ( all => [qw(
 our @EXPORT_OK = ( @{$EXPORT_TAGS{all}} );
 our @EXPORT = qw();
 
-our $VERSION = '0.90';
+our $VERSION = '0.90.1';
 
 # AUTOLOAD {{{
 sub AUTOLOAD {
