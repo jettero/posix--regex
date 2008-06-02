e377ab59e292a5f168df25163ba0842abfa41d2e
Paul Miller <paul@voltar.org>
2008-06-02 08:27:19 -0400

    reversioned

--- Regex.pm
+++ Regex.pm
@@ -17,7 +17,7 @@ our %EXPORT_TAGS = ( all => [qw(
 our @EXPORT_OK = ( @{$EXPORT_TAGS{all}} );
 our @EXPORT = qw();
 
-use version; our $VERSION = qv('0.90.10');
+use version; our $VERSION = qv('0.90.11');
 
 # AUTOLOAD {{{
 sub AUTOLOAD {
