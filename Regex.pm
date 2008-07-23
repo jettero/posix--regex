c9f34061f97e1b2759a636b77451c2fb3dc361aa
Paul Miller <paul@voltar.org>
2008-07-23 16:13:15 -0400

    m::b(2)

--- Regex.pm
+++ Regex.pm
@@ -17,7 +17,7 @@ our %EXPORT_TAGS = ( all => [qw(
 our @EXPORT_OK = ( @{$EXPORT_TAGS{all}} );
 our @EXPORT = qw();
 
-use version; our $VERSION = qv('0.90.12');
+use version; our $VERSION = qv('0.90.13');
 
 # AUTOLOAD {{{
 sub AUTOLOAD {
